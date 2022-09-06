2080da9750886d6193d56a5d8cc8d5707355921b
Xiphe <github@xiphe.net>
2022-09-06 12:51:55 +0200

    fix: push additional tags as semantic-release plugin

--- release.config.js
+++ release.config.js
@@ -1,5 +1,7 @@
-const { major, minor, prerelease } = require("semver");
+const { promisify } = require("node:util");
+const exec = promisify(require("node:child_process").exec);
 
+/** @type {import('semantic-release').Options} */
 module.exports = {
   branches: ["+([0-9])?(.{+([0-9]),x}).x", "main", "next"],
   plugins: [
@@ -7,25 +9,26 @@ module.exports = {
     "@semantic-release/release-notes-generator",
     "@semantic-release/github",
     {
-      async publishSuccessful(version) {
-        if (prerelease(version)) {
-          this.log.info(
+      /**
+       * @param {*} _
+       * @param {import('semantic-release').Context} param1
+       */
+      success(_, { nextRelease: { version }, logger }) {
+        if (version.match(/[^0-9\.]/)) {
+          logger.info(
             "Prerelease version detected; will not add a major version tag."
           );
           return;
         }
-        const versionTags = [
-          major(version),
-          `${major(version)}.${minor(version)}`,
-        ];
-        for (const v of versionTags) {
-          this.log.info(`Deleting version tag ${v} from origin.`);
-          await this.shell.exec(`git push origin :refs/tags/v${v}`);
+        const [major, minor] = version.split(".");
 
-          this.log.info(`Pushing new version tag ${v} to git.`);
-          await this.shell.exec(`git tag --force v${v}`);
-          await this.shell.exec(`git push origin v${v}`);
-        }
+        return Promise.all(
+          [major, `${major}.${minor}`].map(async (v) => {
+            logger.info(`Pushing new version tag ${v} to git.`);
+            await exec(`git tag --force v${v}`);
+            await exec(`git push origin v${v} --force`);
+          })
+        );
       },
     },
   ],
