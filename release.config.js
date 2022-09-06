2922333f91a6ca67e2794d2be903691e16f5f8f6
Xiphe <github@xiphe.net>
2022-09-06 11:03:13 +0200

    feat: initial implementation

---
+++ release.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  branches: ["+([0-9])?(.{+([0-9]),x}).x", "main", "next"],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/github",
+  ],
+};
