b14a64d9ffe93b247fa870a444480a1d77adc7e1
Xiphe <github@xiphe.net>
2023-05-31 13:44:48 +0200

    feat: include changelog in prerelease comment

--- release.config.template.js
+++ release.config.template.js
@@ -1,13 +1,3 @@
-const PR_COMMENT_TEMPLATE = `## Test this PR 🧪
-
-- version \`<%= version %>\`
-- published at **<% print(date.format('ddd, MMM DD YYYY - HH:mm:ss [GMT]Z')) %>**
-- based on <%= gitHead %>
-
-\`\`\`bash
-yarn add <%= name %>@<%= channel %>
-\`\`\``;
-
 module.exports = {
   branches: [
     "+([0-9])?(.{+([0-9]),x}).x",
@@ -24,8 +14,28 @@ module.exports = {
     "@semantic-release/npm",
     process.env.PRE_RELEASE && [
       "decorate-gh-pr/on-release",
-      { prepend: false, comment: PR_COMMENT_TEMPLATE },
+      {
+        prepend: false,
+        comment,
+      },
     ],
     !process.env.PRE_RELEASE && "@semantic-release/github",
   ].filter(Boolean),
 };
+
+function comment({ version, name, date, gitHead, channel, notes }) {
+  return `
+## Test this PR 🧪
+
+- version \`${version}\`
+- published at **${date.format("ddd, MMM DD YYYY - HH:mm:ss [GMT]Z")}**
+- based on ${gitHead}
+
+\`\`\`bash
+yarn add ${name}@${version}
+# or yarn add ${name}@${channel}
+\`\`\`
+
+## Changelog for ${notes.replace(/^#+ /, "")}
+`.trim();
+}
